534e3fde0dc0b39d47779dccab665b7c52463273
NikolayPostanogov <postanogov@nwtn-group.com>
2022-04-24 23:44:33 +0500

    added faker

---
+++ src/test/java/qa/guru/tests/RegistrationFormWithFakerTests.java
@@ -0,0 +1,92 @@
+package qa.guru.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.format;
+import static qa.guru.utils.RandomUtils.getRandomEmail;
+import static qa.guru.utils.RandomUtils.getRandomString;
+
+public class RegistrationFormWithFakerTests {
+    Faker faker = new Faker(new Locale("en"));
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            gender = "Male",
+            phoneNumber = "0000000000",
+            year = "2000",
+            month = "May",
+            day = "17", //в формате mm
+            subject = "Maths",
+            address = faker.address().fullAddress(),
+            hobby = "Sports",
+            imgName = "img.png",
+            imgPath = "img/" + imgName,
+            state = "NCR",
+            city = "Delhi";
+
+    String expectedFullName = format("%s %s", firstName, lastName);
+    String expectedDate = format("%s %s,%s",day,month,year);
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterAll
+    static void setDown() {
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @Test
+    void formTests() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phoneNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0"+day+":not(react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#currentAddress").setValue(address);
+        $("#hobbiesWrapper").$(byText(hobby)).click();
+        $("#uploadPicture").uploadFromClasspath(imgPath);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(
+
+                text(expectedFullName),
+                text(email),
+                text(gender),
+                text(phoneNumber),
+                text(expectedDate),
+                text(subject),
+                text(hobby),
+                text(imgName),
+                text(address),
+                text(state + " " + city)
+        );
+    }
+}
